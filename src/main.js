ec2320667291d597dfe17488064c12636364fdcb
zhaojinjie <zhaojinjie@hcylsoft.cn>
2020-12-26 15:43:23 +0800

    init project

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './plugins/element.js'
 
 Vue.config.productionTip = false
 
 new Vue({
+  // 通过router 配置参数输入路由
+  // 从而让整个应用都有路由功能
   router,
+  
   render: h => h(App)
 }).$mount('#app')
