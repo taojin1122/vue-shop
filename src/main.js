90d7e4a63bc435407f5b185644e951a843326423
zhaojinjie <zhaojinjie@hcylsoft.cn>
2021-01-16 14:56:08 +0800

    代码格式

--- src/main.js
+++ src/main.js
@@ -45,7 +45,7 @@ Vue.filter('dateFormat', function(originValue) {
 
   const y = dt.getFullYear()
   const m = (dt.getMonth() + 1 + '').padStart(2, '0')
-  const d = (dt.getDate() + '').padStart(2,'0')
+  const d = (dt.getDate() + '').padStart(2, '0')
 
   const hh = (dt.getHours() + '').padStart(2, '0')
   const mm = (dt.getMinutes() + '').padStart(2, '0')
